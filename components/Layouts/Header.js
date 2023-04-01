0ccc50d59c0ee92eba89ec93002a88542885edd5
raf256 <1rafaelmodesto@gmail.com>
2023-04-01 14:08:53 -0400

    added p5 sketches and other layout things

--- components/Layouts/Header.js
+++ components/Layouts/Header.js
@@ -31,10 +31,10 @@ const Navbar = () => {
         {isOpen ? (
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
+            className="h-6 w-6 "
             fill="none"
             viewBox="0 0 24 24"
-            stroke="currentColor"
+            stroke="white"
           >
             <path
               strokeLinecap="round"
@@ -49,7 +49,7 @@ const Navbar = () => {
             className="h-6 w-6"
             fill="none"
             viewBox="0 0 24 24"
-            stroke="currentColor"
+            stroke="white"
           >
             <path
               strokeLinecap="round"
